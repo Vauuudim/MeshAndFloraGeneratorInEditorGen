37bc22d566c6944658e5c376a2f2819d5a90a5b6
Vauuudim <66196372+Vauuudim@users.noreply.github.com>
2021-01-14 15:58:34 +1000

    Add files via upload

---
+++ 13.01.2021/Scripts/MeshCombine.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshCombine : MonoBehaviour
+{
+    void Start()
+    {
+         MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+         for (int i = 0; i < meshFilters.Length; i++)
+         {
+             combine[i].mesh = meshFilters[i].sharedMesh;
+             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+             meshFilters[i].gameObject.SetActive(false);
+         }
+         transform.GetComponent<MeshFilter>().mesh = new Mesh();
+         transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+         transform.gameObject.SetActive(true);
+     }
+
+
+     void Update()
+     {
+
+     }
+
+     /*
+     void CombineMeshes()
+     {
+         MeshFilter[] meshFilters = GetComponentInChildren<MeshFilter>();
+         CombineInstance[] combines = new CombineInstance[meshFilters.Length];
+
+         for (int i = 0; i < meshFilters.Length; i++)
+         {
+             combines[i].mesh = meshFilters[i].sharedMesh;
+             combines[i].transform = meshFilters[i].transform.localToWorldMatrix;
+             meshFilters[i].gameObject.SetActive(false);
+         }
+         MeshFilter meshFiltere = transform.getComponent<MeshFilter>();
+         meshFiltere.mesh = new Mesh();
+         meshFiltere.mesh.CombineMeshes(combines);
+         GetComponent<MeshCollider>().sharedMesh = meshFiltere.mesh;
+         transform.gameObject.SetActive(true);
+     }*/
+
+    }
